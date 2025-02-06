8e609290297fa8055f174c9eeacc35dbb4f0e0d9
fitsum-gs <155555669+fitsum-gs@users.noreply.github.com>
2025-02-06 09:34:41 -0800

    Create police recruits.py

---
+++ police recruits.py
@@ -0,0 +1,13 @@
+x=int(input())
+events=list(map(int,input().split()))
+police=0
+untreated=0
+for event in events:
+    if event==-1:
+        if police>0:
+            police-=1
+        else:
+            untreated+=1
+    else:
+        police+=event
+print(untreated)
